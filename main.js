f5d4826b90109e1428b58ecae32b4df7d0d5219e
Jose Perez Merino <126683072+joseperezm@users.noreply.github.com>
2023-05-01 23:47:26 -0400

    usuario lleva menos tiempo

--- main.js
+++ main.js
@@ -9,36 +9,40 @@ function calcularTiempoParaSiguienteGrado(){
     let gradoSiguiente = "";
 
     if(gradoActual === "blanco"){
-      if(añosEntrenando < 1){
-        añosParaSiguienteGrado = 1 - añosEntrenando;
-        gradoSiguiente = "blanco";
-      } else {
+      if(añosEntrenando >= 0){
         añosParaSiguienteGrado = 2 - añosEntrenando;
         gradoSiguiente = "azul";
+      } else {
+        alert("Debe haber ingresado un número positivo de años.");
+        seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
+        continue;
       }
     } else if(gradoActual === "azul"){
       if(añosEntrenando >= 2){
         añosParaSiguienteGrado = 5 - añosEntrenando;
         gradoSiguiente = "purpura";
       } else {
-        añosParaSiguienteGrado = 2 - añosEntrenando;
-        gradoSiguiente = "azul";
+        alert("No llevas el tiempo suficiente para tener el grado azul...");
+        seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
+        continue;
       }
     } else if(gradoActual === "purpura"){
       if(añosEntrenando >= 5){
         añosParaSiguienteGrado = 7 - añosEntrenando;
         gradoSiguiente = "cafe";
       } else {
-        añosParaSiguienteGrado = 5 - añosEntrenando;
-        gradoSiguiente = "purpura";
+        alert("No llevas el tiempo suficiente para tener el grado purpura...");
+        seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
+        continue;
       }
     } else if(gradoActual === "cafe"){
       if(añosEntrenando >= 7){
         añosParaSiguienteGrado = 10 - añosEntrenando;
         gradoSiguiente = "negro";
       } else {
-        añosParaSiguienteGrado = 7 - añosEntrenando;
-        gradoSiguiente = "cafe";
+        alert("No llevas el tiempo suficiente para tener el grado cafe...");
+        seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
+        continue;
       }
     } else if(gradoActual === "negro"){
       alert("¡Felicidades, ya eres cinturón negro!");
@@ -49,11 +53,13 @@ function calcularTiempoParaSiguienteGrado(){
       seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
       continue;
     }
+
     if(añosParaSiguienteGrado >= 0) {
-      alert(`Te faltan ${añosParaSiguienteGrado} años para obtener el grado ${gradoSiguiente}.`);
+      alert(`Te faltan ${añosParaSiguienteGrado} años para obtener el grado ${gradoSiguiente}. ¡Vamos que se puede!`);
     } else {
-      alert(`Ya has superado el tiempo necesario para obtener el grado ${gradoSiguiente}. Habla con tu instructor.`);
+      alert(`¡Ya has superado el tiempo necesario para obtener el grado ${gradoSiguiente}! Te recomiendo hables con tu instructor...`);
     }
+
     seguirCalculando = confirm("¿Deseas calcular el tiempo para el siguiente grado de nuevo?");
   }
 }
